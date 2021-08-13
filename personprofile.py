c62b2a855d8af71a7230c54730cb4e5aadee1493
Gabriel Okamoto <gabriel.okamoto@intuitivecare.com>
2021-08-13 19:33:04 -0300

    day enum

---
+++ personprofile.py
@@ -0,0 +1,16 @@
+# elevatorsim
+# @File:   personprofile.py
+# @Time:   13/08/2021
+# @Author: Gabriel O.
+
+from enum import Enum, auto
+
+
+class Day(Enum):
+    MONDAY = auto()
+    TUESDAY = auto()
+    WEDNESDAY = auto()
+    THURSDAY = auto()
+    FRIDAY = auto()
+    SATURDAY = auto()
+    SUNDAY = auto()
